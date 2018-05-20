89e69b800dd72c966bb726b44d7df7dff8c39453
nick <nicnhines3@gmail.com>
2018-05-19 15:13:10 -1000

    basic splash w/o styling

---
+++ app/components/Login/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+
+
+export default class Login extends Component {
+  render() {
+    return (
+      <View style={styles.wrapper}>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: 'blue',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
